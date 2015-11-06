c2d4d79d38a8366676abb7cfb477e7781576a612
himans91 <himanshu15@hotmail.com>
2015-11-06 13:43:23 +0100

    Navbar flows over the content users are available iafter project search

--- resources/views/projectresult.blade.php
+++ resources/views/projectresult.blade.php
@@ -30,6 +30,18 @@
             </div>  {{-- end of "list-of-projects" --}}
             
          @endforeach
-
    @endif
+      @foreach($users as $user) 
+      <div class="assign-members">   
+         <input type="checkbox" id="cb1" /> 
+      <div class="member--details">
+          <p>{{$user->firstname[0]}}.{{ $user->surname }}</p>
+         <a href=""><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+           <p hidden >{{$user->email}} </p></a>
+           <p>{{$user->tel}}</p>
+      </div>
+      </div><!--End of user-->
+   @endforeach 
+
+
 @stop
